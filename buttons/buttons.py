8b2b8048f1815d3d1cb2a0cf3f486d44e0d2e093
David Lawrence <32286773+david-mk-lawrence@users.noreply.github.com>
2022-02-01 20:03:36 -0800

    handle http errors

--- buttons/buttons.py
+++ buttons/buttons.py
@@ -12,7 +12,6 @@ GPIO.setup(PREV, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(PAUSE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(NEXT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-http = urllib3.PoolManager()
 
 def listen(api_url):
     try:
@@ -28,8 +27,16 @@ def listen(api_url):
 def handle_input(pin, last_state, url):
     if GPIO.input(pin) == False:
         if not last_state:
-            print(url)
-            # http.request("PUT", url)
+            send_state(url)
         return True
     else:
         return False
+
+def send_state(url):
+    try:
+        http = urllib3.PoolManager()
+        resp = http.request("PUT", url)
+        if resp.status != 200:
+            raise Exception("failed to send state")
+    except Exception as err:
+        print(err)
